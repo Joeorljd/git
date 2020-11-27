14193c74fff5167c3e45a75f3e5ab39761de6a05
Joeorljd <75114527+Joeorljd@users.noreply.github.com>
2020-11-27 20:04:48 +0800

    Update TestGit.java

--- src/main/java/com/atguigu/TestGit.java
+++ src/main/java/com/atguigu/TestGit.java
@@ -10,5 +10,6 @@ public class TestGit {
         System.out.println("变色");
         System.out.println("hot-fix");
         System.out.println("push");
+        System.out.println("pull");
     }
 }
